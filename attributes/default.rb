b5359b22ba3830d3200dd4001641554dda40c9a2
Tom Noonan II <thomas.noonan@rackspace.com>
2014-03-17 04:31:30 -0500

    Readme and license and things for publishing

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,11 +2,20 @@
 # Cookbook Name:: afterdark-webhead
 # Attributes:: default
 #
-# Copyright (C) 2014 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
+# Copyright 2014, Rackspace, US Inc.
 #
-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 default['afterdark-webhead']['content-user']  = 'ad-content'
 default['afterdark-webhead']['content-group'] = 'ad-content'
