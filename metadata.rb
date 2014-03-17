6bcf6e4a528347119e75eccb79371ae6778838bc
Tom Noonan II <thomas.noonan@rackspace.com>
2014-03-17 05:09:41 -0500

    Reject FORWARD traffic

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'thomas.noonan@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures afterdark-webhead'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "rackspace_nginx", "~> 3.1"
 depends "rackspace_iptables", "~> 1.2"
--- recipes/acl.rb
+++ recipes/acl.rb
@@ -21,6 +21,7 @@ add_iptables_rule('INPUT', '-m state --state RELATED,ESTABLISHED -j ACCEPT', 100
 add_iptables_rule('INPUT', '-i lo -j ACCEPT', 1000, 'Allow Loopback')
 add_iptables_rule('INPUT', "-s #{node["ipaddress"]}/32 -j ACCEPT", 1, 'Allow Ourself')
 add_iptables_rule('INPUT', '-s 0.0.0.0/0 -j REJECT', 0)
+add_iptables_rule('FORWARD', '-s 0.0.0.0/0 -j REJECT', 0)
 
 add_iptables_rule('INPUT', '-p tcp -m tcp --dport 22 -j ACCEPT', 800, 'Open SSH globally')
 add_iptables_rule('INPUT', '-p tcp -m tcp --dport 80 -j ACCEPT', 800, 'Open HTTP globally')
